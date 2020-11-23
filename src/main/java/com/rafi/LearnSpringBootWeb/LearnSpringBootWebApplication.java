fb9af1c552e47b5e88bc9b2f9fe888b94380aee0
rafi-windows <rafi_n_o@github.com>
2020-11-23 11:22:45 +0700

    first commit

---
+++ src/main/java/com/rafi/LearnSpringBootWeb/LearnSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.rafi.LearnSpringBootWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootWebApplication.class, args);
+	}
+
+}
