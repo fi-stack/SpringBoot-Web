9106094c7edcd801910bf2e0dbbbfdb67c143421
rafi-windows <rafi_n_o@github.com>
2020-11-23 18:03:50 +0700

    Create table connect DB MySQL JPA

---
+++ src/main/java/com/rafi/LearnSpringBootWeb/model/Mahasiswa.java
@@ -0,0 +1,56 @@
+package com.rafi.LearnSpringBootWeb.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Mahasiswa {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nama;
+    private Integer nim;
+    private String email;
+    private String jurusan;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Integer getNim() {
+        return nim;
+    }
+
+    public void setNim(Integer nim) {
+        this.nim = nim;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+}
